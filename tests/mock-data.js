5cc5a13bbf3fe51933e33d68995d003b8702ffbf
Ganesh Annan <gannan@digitalbazaar.com>
2018-12-06 10:16:32 -0500

    Add did doc mock data.

--- tests/mock-data.js
+++ tests/mock-data.js
@@ -3,6 +3,7 @@ const mock = {};
 module.exports = mock;
 
 const capabilities = mock.capabilities = {};
+const ddocs = mock.ddocs = {};
 const owners = mock.owners = {};
 const _loaderData = {};
 
@@ -12,6 +13,11 @@ owners.alice = require('./mock-documents/ed25519-alice-keys');
 owners.bob = require('./mock-documents/ed25519-bob-keys');
 owners.carol = require('./mock-documents/ed25519-carol-keys');
 owners.diana = require('./mock-documents/ed25519-diana-keys');
+
+ddocs.alpha = require('./mock-documents/did-doc-alpha');
+ddocs.beta = require('./mock-documents/did-doc-beta');
+ddocs.gamma = require('./mock-documents/did-doc-gamma');
+ddocs.delta = require('./mock-documents/did-doc-delta');
 
 capabilities.root = {};
 capabilities.root.controller = {
@@ -28,17 +34,29 @@ capabilities.root.keys = {
 };
 
 // Generate a flattened list of all keys
-let keyList = Object.keys(owners).map(name => KEY_TYPES
+let ownersKeyList = Object.keys(owners).map(name => KEY_TYPES
   .map(keyType => owners[name][keyType])
   .reduce((acc, curr) => acc.concat(curr), [])
 );
-keyList = [].concat.apply([], keyList)
+ownersKeyList = [].concat.apply([], ownersKeyList)
   .filter(key => !!key && typeof key.publicKey !== 'string')
   .map((doc) => doc.publicKey ? doc.publicKey : doc);
 
+let ddocKeyList = Object.keys(ddocs).map(name => KEY_TYPES
+  .map(keyType => ddocs[name][keyType])
+  .reduce((acc, curr) => acc.concat(curr), [])
+);
+ddocKeyList = [].concat.apply([], ddocKeyList)
+  .filter(key => !!key && typeof key.publicKey !== 'string')
+  .map(doc => doc.publicKey ? doc.publicKey : doc)
+  .reduce((acc, curr) => acc.concat(curr), []);
+
+const keyList = ownersKeyList.concat(ddocKeyList);
+
 mock.addToLoader = ({doc}) => {
   if(doc.id in _loaderData) {
-    throw new Error('ID of document has already been registered in the loader');
+    throw new Error(
+      `ID of document has already been registered in the loader: ${doc.id}`);
   }
   _loaderData[doc.id] = doc;
 };

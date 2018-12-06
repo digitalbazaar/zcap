971682807cbc7d64e060e5a85605a326ba53fd25
Ganesh Annan <gannan@digitalbazaar.com>
2018-12-06 10:16:32 -0500

    Add did doc mock data.

---
+++ tests/mock-documents/did-doc-delta.js
@@ -0,0 +1,46 @@
+module.exports = {
+  "@context": "https://w3id.org/veres-one/v1",
+  "id": "did:v1:test:nym:9iofLVptQB9BwZ484xTHbvwfxEmN2ByNR8hSXE2uho88",
+  "authentication": [
+    {
+      "type": "Ed25519SignatureAuthentication2018",
+      "publicKey": [
+        {
+          "id": "did:v1:test:nym:9iofLVptQB9BwZ484xTHbvwfxEmN2ByNR8hSXE2uho88#authn-key-1",
+          "type": "Ed25519VerificationKey2018",
+          "owner": "did:v1:test:nym:9iofLVptQB9BwZ484xTHbvwfxEmN2ByNR8hSXE2uho88",
+          "publicKeyBase58": "9iofLVptQB9BwZ484xTHbvwfxEmN2ByNR8hSXE2uho88",
+          "privateKeyBase58": "38aVNnnSMwoCoENhtRdxriZXbJnezeqvbTNiromuBrD6HgADSXDkPA5mnuinoikNimF3u2Eu6P7ATGXtezDzqYHi"
+        }
+      ]
+    }
+  ],
+  "capabilityDelegation": [
+    {
+      "type": "Ed25519SignatureCapabilityAuthorization2018",
+      "publicKey": [
+        {
+          "id": "did:v1:test:nym:9iofLVptQB9BwZ484xTHbvwfxEmN2ByNR8hSXE2uho88#ocap-grant-key-1",
+          "type": "Ed25519VerificationKey2018",
+          "owner": "did:v1:test:nym:9iofLVptQB9BwZ484xTHbvwfxEmN2ByNR8hSXE2uho88",
+          "publicKeyBase58": "5F3zHUr8xpVzS1W7tGWTEfaybTQZmVJkuNH49Y89ryj8",
+          "privateKeyBase58": "39396uTD1gZBmSzpYUXreL2KvEhDZypdT8DvMueHFAkbHa9GCQH7qqK51SMKMUjaoi2nXiYKik6LMVLRkbYvCq3Q"
+        }
+      ]
+    }
+  ],
+  "capabilityInvocation": [
+    {
+      "type": "Ed25519SignatureCapabilityAuthorization2018",
+      "publicKey": [
+        {
+          "id": "did:v1:test:nym:9iofLVptQB9BwZ484xTHbvwfxEmN2ByNR8hSXE2uho88#ocap-invoke-key-1",
+          "type": "Ed25519VerificationKey2018",
+          "owner": "did:v1:test:nym:9iofLVptQB9BwZ484xTHbvwfxEmN2ByNR8hSXE2uho88",
+          "publicKeyBase58": "7Am8p94Sbi1bvmLych2FNvBdrEsyhbiqcCtWR6ztg2ho",
+          "privateKeyBase58": "3sVpdhTCguiuWs6qxLAgpeejJbAAAehUdKXkS7fjkqMxocUzbzwBQuqy7G3AQ5SAKZBZWHeJaRjwuKPFEu3yfYcH"
+        }
+      ]
+    }
+  ]
+};

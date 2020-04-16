8f30dfb48fad598d9611f018ac72331ab379c063
Matthew Collier <mcollier@digitalbazaar.com>
2020-04-16 11:52:56 -0400

    Fix linting errors.

--- tests/helpers.js
+++ tests/helpers.js
@@ -16,5 +16,7 @@ api.Controller = class Controller {
   }
 };
 
+/* eslint-disable */
 const b = a=>a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,b);
 api.uuid = () => `urn:uuid:${b()}`;
+/* eslint-enable */

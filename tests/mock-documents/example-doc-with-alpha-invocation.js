7ec3bbb3ea8bef8d3fdcbecbaf51a7391459c772
Dave Longley <dlongley@digitalbazaar.com>
2021-07-01 18:13:05 -0400

    Require `invocationTarget` in zcap invocation proofs.

--- tests/mock-documents/example-doc-with-alpha-invocation.js
+++ tests/mock-documents/example-doc-with-alpha-invocation.js
@@ -6,7 +6,8 @@ module.exports = {
     "type": "Ed25519Signature2018",
     "created": "2018-02-13T21:26:08Z",
     "capability": "https://example.org/alice/caps#1",
-    "jws": "eyJhbGciOiJFZERTQSIsImI2NCI6ZmFsc2UsImNyaXQiOlsiYjY0Il19..BRtc_VU1Txb-wGVq1VqTLXjPQQCeT7xj8BidGh8DdW0Z-6g0DCvPY_FH7sHRrphlmIRgSe59W4p1YeYLdAjcDg",
+    "invocationTarget": "https://example.org/alice/caps#1",
+    "jws": "eyJhbGciOiJFZERTQSIsImI2NCI6ZmFsc2UsImNyaXQiOlsiYjY0Il19..nVu3UF6oeDWkbbbvhUe0KCzr6jLzdjHKRpicxB9z04F4X3XIFmFrfCiMIrPlH068HK18g4InUksrlXJbonYyCg",
     "proofPurpose": "capabilityInvocation",
     "verificationMethod": "https://example.com/i/alice/keys/1"
   }
--- tests/mock-documents/example-doc-with-beta-invocation.js
+++ tests/mock-documents/example-doc-with-beta-invocation.js
@@ -6,7 +6,8 @@ module.exports = {
     "type": "Ed25519Signature2018",
     "created": "2018-02-13T21:26:08Z",
     "capability": "https://example.org/alice/caps#0",
-    "jws": "eyJhbGciOiJFZERTQSIsImI2NCI6ZmFsc2UsImNyaXQiOlsiYjY0Il19..nW6G_xUHxK49mwimQlcfhSmjGvTc3FcFzopzusBwsz-EYN_WNM4rQWPBcWG6gmLxk9QES1bude5NhSRXWgwVCw",
+    "invocationTarget": "https://example.org/alice/caps#0",
+    "jws": "eyJhbGciOiJFZERTQSIsImI2NCI6ZmFsc2UsImNyaXQiOlsiYjY0Il19..QyM223BkvuaSE9pLNq9lFbSAa6gI5yEGAZ6a4lGaf25FuDaphDUdzJQLyqA5dSVgcyBDe1GVoPfTN_d7R23UCA",
     "proofPurpose": "capabilityInvocation",
     "verificationMethod": "https://example.com/i/alice/keys/1"
   }

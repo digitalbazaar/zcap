83399574527925777cdebeb16c7ad28b58ee98e0
Dave Longley <dlongley@digitalbazaar.com>
2024-03-29 15:11:47 -0400

    Update dev dependencies and fix linting errors.

--- tests/test.js
+++ tests/test.js
@@ -1,15 +1,15 @@
 /**
  * Node.js test runner for zcap.
  *
- * Copyright (c) 2011-2022 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2011-2024 Digital Bazaar, Inc. All rights reserved.
  */
+import * as zcap from '../lib/index.js';
 import chai from 'chai';
 import common from './test-common.js';
 import jsigs from 'jsonld-signatures';
-import * as zcap from '../lib/index.js';
 
-import * as mock from './mock-data.js';
 import * as helpers from './helpers.js';
+import * as mock from './mock-data.js';
 
 const expect = chai.expect;
 
